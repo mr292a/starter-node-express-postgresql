829319e64e56f598b29fc1b59282aea1eb07ffa3
Michael Rosa Jr <mrosa2000@yahoo.com>
2022-10-03 17:00:32 -0500

    Added file via upload.

---
+++ src/products/products.service.js
@@ -0,0 +1,13 @@
+const knex = require("../db/connection");
+
+function read(productId) {
+    return knex("products").select("*").where({product_id: productId}).first();
+};
+
+function list() {
+    return knex("products").select("*");
+};
+
+module.exports = {
+    list,
+};
